0aaffca6d998514750dbd34da0ee31ca9c7e5e2b
john2796 <jbmiranda22796@gmail.com>
2019-01-23 09:05:08 -0800

    added delete req just need update post

--- friends/src/App.js
+++ friends/src/App.js
@@ -3,6 +3,11 @@ import "./App.css";
 import axios from "axios";
 
 const URL = "http://localhost:5000/friends";
+
+function validateEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
 class App extends Component {
   state = {
     data: [],
@@ -20,11 +25,19 @@ class App extends Component {
 
   submitHandler = e => {
     e.preventDefault();
+    if (!validateEmail(this.state.email)) {
+      alert("email not valid");
+    }
     const newPost = {
       age: this.state.age,
       name: this.state.name,
       email: this.state.email
     };
+    console.log(newPost);
+    axios
+      .post(`${URL}`, newPost)
+      .then(res => this.setState({ data: res.data }))
+      .catch(err => this.setState({ errors: err }));
   };
 
   componentDidMount() {
@@ -38,6 +51,13 @@ class App extends Component {
       .catch(err => this.setState({ errors: err }));
   };
 
+  deletePost = id => {
+    axios
+      .delete(`${URL}/${id}`)
+      .then(res => this.setState({ data: res.data }))
+      .catch(err => this.setState({ errors: err }));
+  };
+
   render() {
     const { data } = this.state;
     const items = data.map(item => {
@@ -46,6 +66,7 @@ class App extends Component {
           <p>{item.age}</p>
           <p>{item.email}</p>
           <p>{item.name}</p>
+          <button onClick={() => this.deletePost(item.id)}>delete</button>
         </li>
       );
     });
@@ -58,6 +79,7 @@ class App extends Component {
             name="name"
             placeholder="name"
             onChange={this.changeHandler}
+            value={this.state.name}
           />{" "}
           <br />
           <input
@@ -65,6 +87,7 @@ class App extends Component {
             name="email"
             placeholder="email"
             onChange={this.changeHandler}
+            value={this.state.email}
           />{" "}
           <br />
           <input
@@ -72,10 +95,16 @@ class App extends Component {
             name="age"
             placeholder="age"
             onChange={this.changeHandler}
+            value={this.state.age}
           />{" "}
           <br />
+          <button type="submit">submit</button>
         </form>
-        <ul>{items}</ul>
+        <ul>
+          <hr />
+          <h1>Lambda Councils</h1>
+          {items}
+        </ul>
       </div>
     );
   }
