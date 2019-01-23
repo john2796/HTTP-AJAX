a7edfa89e3e355d8eaaf87bc23961ec26861aa9d
john2796 <jbmiranda22796@gmail.com>
2019-01-23 08:46:49 -0800

    completed get req working on post with form

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,81 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
+const URL = "http://localhost:5000/friends";
 class App extends Component {
+  state = {
+    data: [],
+    errors: {},
+    age: "",
+    name: "",
+    email: ""
+  };
+
+  changeHandler = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  submitHandler = e => {
+    e.preventDefault();
+    const newPost = {
+      age: this.state.age,
+      name: this.state.name,
+      email: this.state.email
+    };
+  };
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = () => {
+    axios
+      .get(`${URL}`)
+      .then(res => this.setState({ data: res.data }))
+      .catch(err => this.setState({ errors: err }));
+  };
+
   render() {
+    const { data } = this.state;
+    const items = data.map(item => {
+      return (
+        <li key={item.id}>
+          <p>{item.age}</p>
+          <p>{item.email}</p>
+          <p>{item.name}</p>
+        </li>
+      );
+    });
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form onSubmit={this.submitHandler}>
+          <p>Form</p>
+          <input
+            type="text"
+            name="name"
+            placeholder="name"
+            onChange={this.changeHandler}
+          />{" "}
+          <br />
+          <input
+            type="text"
+            name="email"
+            placeholder="email"
+            onChange={this.changeHandler}
+          />{" "}
+          <br />
+          <input
+            type="text"
+            name="age"
+            placeholder="age"
+            onChange={this.changeHandler}
+          />{" "}
+          <br />
+        </form>
+        <ul>{items}</ul>
       </div>
     );
   }
